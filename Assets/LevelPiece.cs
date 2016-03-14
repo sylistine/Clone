8157e8c9c9605d5b6b6922002238e110e44a07c4
Aaron Hull <aaron@10hpgames.com>
2016-03-14 21:36:36 +0900

    Added some real _dumb_ procedural level gen

---
+++ Assets/LevelPiece.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPiece : MonoBehaviour
+{
+    public Vector3 center
+    {
+        get
+        {
+            return this.transform.position;
+        }
+    }
+
+    [System.Serializable]
+    public class Edge
+    {
+        public float distance;
+        public Vector3 direction;
+    }
+
+    public Edge[] edges;
+
+    void OnDrawGizmos()
+    {
+        for(int i = 0; i < edges.Length; i++)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(this.transform.position + edges[i].direction * edges[i].distance, 0.5f);
+        }
+    }
+}
