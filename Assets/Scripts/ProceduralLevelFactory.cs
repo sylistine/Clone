2d0816ac12d3dcb411bfb4656d00227b781db495
Aaron Hull <aaron@10hpgames.com>
2016-03-23 18:57:05 -0700

    fixed procedurally generated pieces from randomly flipping upsidedown

--- Assets/Scripts/ProceduralLevelFactory.cs
+++ Assets/Scripts/ProceduralLevelFactory.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class ProceduralLevelFactory : MonoBehaviour
 {
+    // Editor variables
     public class Connection
     {
         public Vector3 origin;
@@ -11,6 +12,7 @@ public class ProceduralLevelFactory : MonoBehaviour
     }
     public int levelSize;
     public Transform[] pieces;
+
     Queue<Connection> connections = new Queue<Connection>();
 
     void Start ()
@@ -42,19 +44,27 @@ public class ProceduralLevelFactory : MonoBehaviour
     
     void CreateNewLevelPiece(Connection otherConnection)
     {
+        // Instantiate piece
         LevelPiece newPiece = GameObject.Instantiate(pieces[Random.Range(0, pieces.Length)]).GetComponent<LevelPiece>();
         newPiece.transform.parent = this.transform;
-        // If the connection was always index = 0, the levels would quickly become predictable.
+
+        // get connecting point of new piece
         int firstConnectionIndex = Random.Range(0, newPiece.edges.Length);
+
+        // Establish new piece position based on the original dequeued connection and the connecting point of the new piece.
         newPiece.transform.localPosition = otherConnection.origin + otherConnection.worldDirection * newPiece.edges[firstConnectionIndex].distance;
+        
+        // Create rotation quaternion.
         Vector3 fromRot = newPiece.transform.TransformDirection(newPiece.edges[firstConnectionIndex].localDirection);
         fromRot.y = 0;
         fromRot = fromRot.normalized;
         Vector3 toRot = otherConnection.worldDirection;
         toRot.y = 0;
         toRot = toRot.normalized;
-        if(fromRot != Vector3.zero - toRot)
-            newPiece.transform.rotation = newPiece.transform.rotation * Quaternion.FromToRotation(fromRot, Vector3.zero - toRot);
+        if (fromRot != toRot && fromRot != Vector3.zero - toRot)
+        {
+            newPiece.transform.rotation *= Quaternion.FromToRotation(fromRot, Vector3.zero - toRot);
+        }
         for (int i = 0; i < newPiece.edges.Length; i++)
         {
             if (i == firstConnectionIndex) continue;
