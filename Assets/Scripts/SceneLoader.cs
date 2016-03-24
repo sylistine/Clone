69936a24743ea6f3e65f6f02cc90aec634d50cd7
Aaron Hull <aaron@10hpgames.com>
2016-03-23 18:29:06 -0700

    Added simple title screen.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int sceneNumber;
+
+    public void LoadScene()
+    {
+        SceneManager.UnloadScene(sceneNumber);
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
